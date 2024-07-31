e73218606ace0d1fefcb12847495b996e0daa79a
Akidu Ekanayake <99312096+IT21344824@users.noreply.github.com>
2024-07-31 12:28:54 +0530

    Delete src directory

--- src/routes/SignupRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {signupUser} = require("../controller/SignupController");
-
-const router = express.Router();
-
-router.post("/register", signupUser);
-
-module.exports = router;
